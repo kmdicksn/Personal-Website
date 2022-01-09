fcf7554c9879614e7130e6ae0e0eec2fef2b881f
Dickson Ngan <33165850+kmdicksn@users.noreply.github.com>
2022-01-08 23:31:35 -0800

    add sections

--- src/index.js
+++ src/index.js
@@ -1,14 +1,1 @@
 import * as bootstrap from 'bootstrap';
-
-window.onscroll = function() {
-	let currentScrollPos = window.pageYOffset;
-	let opacity = 1 / currentScrollPos;
-
-	if (opacity > 1) {
-		opacity = 1;
-	}
-
-	document.getElementById("mouse").style.opacity = (opacity).toString()
-
-
-}
