8357eed66c8d8c6811da77479b5ba23bbfc699c9
kmdicksn <dicksonngan101@gmail.com>
2022-01-09 01:28:37 -0800

    add resume link

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import * as bootstrap from 'bootstrap';
+
+const resumeClick = () => {
+	window.open('https://dickson-ngan.s3.us-west-1.amazonaws.com/resume.pdf', '_blank').focus();
+}
+
+window.resumeClick = resumeClick;
