161d900aac622920cf250ce54160e6efac02b79c
Dickson Ngan <33165850+kmdicksn@users.noreply.github.com>
2022-01-09 15:50:31 -0800

    update design

--- src/index.js
+++ src/index.js
@@ -4,4 +4,9 @@ const resumeClick = () => {
 	window.open('https://dickson-ngan.s3.us-west-1.amazonaws.com/resume.pdf', '_blank').focus();
 }
 
+const githubClick = () => {
+	window.open('https://github.com/kmdicksn', '_blank').focus();
+}
+
 window.resumeClick = resumeClick;
+window.githubClick = githubClick;
